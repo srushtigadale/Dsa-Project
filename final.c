42ddb65dbee9c7be859b8f7c26396bcf08862a76
TanmayiSulakhe <149750275+TanmayiSulakhe@users.noreply.github.com>
2024-04-14 23:00:36 +0530

    Create final.c
    made rearrangements

---
+++ final.c
@@ -0,0 +1,390 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ITEMS 100
+#define MAX_NAME_LENGTH 50
+double totalincome = 0;
+/*all queue dunctions for waitlists */
+typedef struct queue_node {
+    int node_number;  // New field to store the node number
+    int num_people;
+    struct queue_node* next;
+} queue_node;
+
+typedef struct {
+    queue_node* front;
+    queue_node* rear;
+    int node_count; // To keep track of the number of nodes
+} queue;
+
+// Initialize an empty queue
+void init_queue(queue* q) {
+    q->front = q->rear = NULL;
+    q->node_count = 0; // Initialize node count
+}
+
+// Check if the queue is empty
+int is_empty(queue* q) {
+    return q->front == NULL;
+}
+// Enqueue a customer into the queue
+void enqueue(queue* q, int num_people) {
+    queue_node* new_node = (queue_node*)malloc(sizeof(queue_node));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->node_number = q->node_count + 1; // Assign node number
+    q->node_count++; // Increment node count
+    new_node->num_people = num_people;
+    new_node->next = NULL;
+    if (is_empty(q)) {
+        q->front = q->rear = new_node;
+    } else {
+        q->rear->next = new_node;
+        q->rear = new_node;
+    }
+}
+
+// Display the customers in the queue along with node numbers
+void display_queue(queue* q) {
+    if (is_empty(q)) {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Waitlist:\n");
+    queue_node* current = q->front;
+    while (current != NULL) {
+        printf("Waitlist number: %d: Group of : %d\n", current->node_number, current->num_people);
+        current = current->next;
+    }
+}
+
+/*end of queue functions for waitlists*/
+
+
+typedef struct queue_nodec {
+    char *itemname;
+    struct queue_nodec* next;
+} queue_nodec;
+
+typedef struct {
+    queue_nodec* front;
+    queue_nodec* rear;
+} queuec;
+
+struct MenuItem {
+    char name[MAX_NAME_LENGTH];
+    double price;
+};
+
+
+typedef struct node{
+    char *ordername;
+    struct node* next;
+    double price;
+}node;
+
+typedef node* list;//taking customer's order in LL
+
+void init_list(list *head){
+    *head = NULL;
+}
+
+
+// void create_node(list *head ,char ordern[] , double p ){
+//     // printf("I'm here");
+//     node *nn = (node*)malloc(sizeof(node));
+//     nn->ordername = (char*)malloc(strlen(ordern));
+//     nn->ordername = ordern;
+//     nn->price = p;
+//     nn->next = NULL;
+//     if(*head == NULL){
+//         *head = nn;
+//         return;
+//     }
+//     node *temp = *head;
+//     while(temp->next != NULL){
+//         // printf("srush");
+//         temp = temp->next;
+//     }
+//     temp->next = nn;
+// }
+
+void create_node(list *head, char ordern[], double p) {
+    node *nn = (node*)malloc(sizeof(node));
+    nn->ordername = (char*)malloc(strlen(ordern) + 1); // Allocate memory for the string
+    strcpy(nn->ordername, ordern); // Copy the string content
+    nn->price = p;
+    nn->next = NULL;
+    if(*head == NULL){
+        *head = nn;
+        return;
+    }
+    node *temp = *head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = nn;
+}
+
+typedef struct table {
+    int occupied;//indicates if table is occupied
+    list order;
+    
+}table;
+
+
+typedef struct section {
+    table* t; //tables in each section
+    int no_of_tables;
+    int chairs;// no. of chair each table has
+} section;
+
+typedef struct restaurant {
+    int no_of_sections;
+    section* array_of_all_sections;
+} restaurant;
+
+void init_section(section* s, int total_tables_in_section, int chair) {
+    s->no_of_tables = total_tables_in_section;
+    s->chairs = chair;
+    s->t = (table*)malloc(sizeof(table) * total_tables_in_section);
+    for (int i = 0; i < total_tables_in_section; i++) {
+        s->t[i].occupied = 0;
+    }
+}
+void init_restaurant(restaurant* r, int sections) {
+    r->no_of_sections = sections;
+    r->array_of_all_sections = (section*)malloc(sizeof(section) * sections);
+    int i = 1;
+    int t = sections;
+    int tb, c;
+    while (t) {
+        printf("\nEnter no.of tables and enter no. of chairs for section %d:  ", i);
+        scanf("%d%d", &tb, &c);
+        init_section(&(r->array_of_all_sections[i - 1]), tb, c);
+        i++;
+        t--;
+    }
+}
+
+
+
+/*
+void init_queuec(queuec* q) {
+    q->front = q->rear = NULL;
+}
+void init_queue1(queue* q) {
+    q->front = q->rear = NULL;
+}
+*/
+void init_queuec(queuec* q) {
+    q->front = q->rear = NULL;
+}
+// void init_queue1(queue* q) {
+//     q->front = q->rear = NULL;
+// }
+
+int is_emptyc(queuec* q) {
+    return q->front == NULL;
+}
+
+void enqueuec(queuec* q, char order_name[]) {
+    queue_nodec* new_node = (queue_nodec*)malloc(sizeof(queue_nodec));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->itemname =(char*)malloc(strlen(order_name));
+    new_node->itemname = order_name;
+    new_node->next = NULL;
+    if (is_emptyc(q)) {
+        q->front = q->rear = new_node;
+    } else {
+        q->rear->next = new_node;
+        q->rear = new_node;
+    }
+}
+
+void take_the_order(restaurant *res,int sec , int t1 , queuec* chefq){
+    int i = 0;
+    printf("%d\n",i);
+    i++;
+    init_list(&(res->array_of_all_sections[sec - 1].t[t1].order));
+    // init_list(&sec->t[t1].order);
+    printf("%dnow opening the file\n",i);
+    i++;
+
+    int c;
+    FILE* fp = fopen("menu.csv", "r");
+    if (!fp) {
+        printf("Error opening the file.\n");
+        return ;
+    }
+
+    printf("%d\n",i);
+    i++;
+
+    struct MenuItem menu[MAX_ITEMS];
+    int itemCount = 0;
+    // Read menu items from the CSV file
+    while (itemCount < MAX_ITEMS && fscanf(fp, "%49[^,],%lf\n", menu[itemCount].name, &menu[itemCount].price) == 2) {
+        //printf("Read item: %s, %.2lf\n", menu[itemCount].name, menu[itemCount].price); // Debug statement
+        itemCount++;
+    }
+    // ...
+    fclose(fp);
+    // Print the menu
+    printf("Today's Menu:\n");
+    printf("\n");
+    for (int i = 0; i < itemCount; i++) {
+            printf("%-30s %.2lf\n", menu[i].name, menu[i].price);
+            printf("\n");
+        }
+    while(1){
+            printf("Enter the number of the item you want to order. Press -1 to stop");
+            scanf("%d" , &c);
+            if(c == -1)
+                break;
+            create_node(&(res->array_of_all_sections[sec - 1].t[t1].order), menu[c - 1].name, menu[c - 1].price);
+            // create_node(res->array_of_all_sections[sec - 1].t[t1].order,menu[c-1].name,menu[c-1].price);
+            // printf("hey");
+            enqueuec(chefq ,menu[c-1].name);
+            // printf("yo");
+            // printf("order is - %s , %d\n" , menu[c-1].name ,(int) menu[c-1].price);
+        }
+}
+
+
+
+void deoccupy_tables(restaurant *r , section *sec , int t1 ,queuec *chefq, queue *waitlist){
+    // sec->
+    sec->t[t1].occupied = 0;
+    double bill = 0;
+    node *temp = sec->t[t1].order;
+    printf("Bill\n");
+    while (temp){
+        printf("%s\t%f\n" , temp->ordername , temp->price);
+        bill += temp->price;
+        temp = temp->next;
+    }
+    printf("Total money to be paid is %f\nThankyou for visiting, Comeagain!" , bill);
+    totalincome += bill;
+    // return ;
+    
+}
+
+
+void occupy_tables(restaurant* r, int num_people, queue *q) {
+    // int canBseated =0;
+    int temp1, temp2;
+    int flag = 0;
+    temp2 = 10000;
+    int chosen_section_index = -1;
+    int table;
+    // Find section with minimum number of chairs greater than or equal to num_people
+    for (int i = 0; i < (*r).no_of_sections; i++) {
+        temp1 = (*r).array_of_all_sections[i].chairs;
+        if (temp1 >= num_people && temp2 > temp1) {
+            temp2 = temp1;
+            table = 1;
+            chosen_section_index = i;
+        }
+    }
+
+    // If no suitable section with single tables is found, check for sections with tables that can accommodate twice the number of people
+    if (chosen_section_index == -1) {
+        for (int i = 0; i < (*r).no_of_sections; i++) {
+            temp1 = (*r).array_of_all_sections[i].chairs * 2;
+            if (temp1 >= num_people && temp2 > temp1) {
+                temp2 = temp1;
+                chosen_section_index = i;
+                table = 2;
+            }
+        }
+    }
+
+    if(chosen_section_index == -1){
+        printf("Sorry, Cannot be seated\n");
+    }
+
+    // If a suitable section is found, occupy the tables
+    if (chosen_section_index != -1) {
+        section* chosen_section = &((*r).array_of_all_sections[chosen_section_index]);
+        //no. of chairs are either required for 1 table or 2 tables
+        if (table == 1) {
+            for (int j = 0; j < chosen_section->no_of_tables; j++) {
+                if (chosen_section->t[j].occupied == 0) {
+                    chosen_section->t[j].occupied = 1;
+                    flag =1;
+                    printf("Table %d allocated in section %d \n", j + 1,chosen_section_index+1);
+                    break;
+                }
+            }
+        }
+        else { //tables req = 2
+            for (int j = 0; j < chosen_section->no_of_tables; j++) {
+                if (chosen_section->t[j].occupied == 0) {
+                    for (int k = j+1; k < chosen_section->no_of_tables; k++) {
+                        if (chosen_section->t[k].occupied == 0) {
+                            chosen_section->t[j].occupied = 1;
+                            chosen_section->t[k].occupied = 1;
+                            printf("Table %d allocated\n", j + 1);
+                            printf("Table %d allocated\n", k + 1);
+                            printf("Tables occupied successfully in section %d!\n", chosen_section_index + 1);
+                            flag =1;
+                            break;
+                            // return; // No need to continue after assigning tables
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //else { printf("Sorry, there are not enough seats available in any section.\n");}
+    if(flag == 0){
+
+        printf("Sorry, there are not enough seats available in any section.\n");
+        enqueue(q,num_people);
+        display_queue(q);
+
+    }
+    
+}
+
+int main() {
+    restaurant r;
+    int section,p,tableno;
+    queue waitlist;
+    init_queue(&waitlist);
+    printf("Enter the number of sections: \n");
+    scanf("%d",&section);
+    init_restaurant(&r, section);
+
+
+           
+    printf("Enter number of people in a group:  ");
+    scanf("%d",&p);
+    occupy_tables(&r, p,&waitlist);
+    
+    queuec chefq;
+
+    init_queuec(&chefq);
+    printf("Enter section to order from  \n");
+    scanf("%d",&section);
+    printf("Enter table no to order from  \n");
+    scanf("%d",&tableno);
+    // take_the_order(&(r.array_of_all_sections[section-1]),tableno,&chefq);
+    // deoccupy_tables(&r,&(r.array_of_all_sections[section-1]),tableno,&chefq,&waitlist);
+    
+
+    take_the_order(&r,section, tableno, &chefq);
+
+    // Deoccupy table
+    // deoccupy_tables(&r, &(r.array_of_all_sections[section - 1]), tableno, &chefq, &waitlist);
+    // occupy_tables(&r, 6);
+    // printf("end");
+    return 0;
+}
